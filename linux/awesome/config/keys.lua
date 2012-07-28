acaa184792a8fb5dffbf93c8f9f8b7f84df30958
José Mota <josemota.net@gmail.com>
2012-07-28 12:56:10 +0100

    Add mouse gestures into awesome.

--- linux/awesome/config/keys.lua
+++ linux/awesome/config/keys.lua
@@ -121,5 +121,10 @@ clientbuttons = awful.util.table.join(
 
 -- Set keys
 root.keys(globalkeys)
--- }}}
 
+-- Mouse bindings
+root.buttons(awful.util.table.join(
+    awful.button({ }, 3, function () mymainmenu:toggle() end),
+    awful.button({ }, 4, awful.tag.viewnext),
+    awful.button({ }, 5, awful.tag.viewprev)
+))
