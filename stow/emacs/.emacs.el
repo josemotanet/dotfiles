ae0c323ec42f70204daa0d2ddbaa5583415b110b
José Mota <jose@josemota.net>
2020-01-28 00:36:14 +0000

    [emacs] Update emacs.el.

--- stow/emacs/.emacs.el
+++ stow/emacs/.emacs.el
@@ -1,1 +1,18 @@
 (org-babel-load-file (concat (getenv "HOME") "/.config/emacs/config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (rjsx-mode treemacs yasnippet-snippets yasnippet scss-mode use-package rg rainbow-mode rainbow-delimiters projectile powerline org-bullets olivetti markdown-mode magit js2-mode hlinum gruvbox-theme general flx fill-column-indicator evil-surround diminish diff-hl counsel))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(markdown-code-face ((t (:inherit nil))))
+ '(org-block-begin-line ((t (:foreground "#777" :background nil))))
+ '(org-block-end-line ((t (:foreground "#777" :background nil))))
+ '(org-document-title ((t (:foreground "#f90")))))
