f0ae78b1c298f9224eaa5daf2eb32e8a4cbbdc5d
José Mota <jose@josemota.net>
2020-02-11 22:24:11 +0000

    [installer] Fix GRUB install.

--- archlinux-installer/chroot.sh
+++ archlinux-installer/chroot.sh
@@ -18,8 +18,8 @@ locale-gen
 echo $HOST > /etc/hostname
 mkinitcpio -P
 echo 'root:'$PASS | chpasswd
-pacman -S --noconfirm grub efibootmgr osprober ntfs-3g
-grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader-id=GRUB
+pacman -S --noconfirm grub efibootmgr os-prober ntfs-3g
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
 echo -e "\e[36m=== Post installation successful.\e[39m"
