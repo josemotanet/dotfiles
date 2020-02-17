6b578360c71a9a9b746ae358c8fb534f34818825
José Mota <jose@josemota.net>
2020-02-17 17:14:30 +0000

    [emacs] Origami mode instead of hs-minor-mode.

--- stow/emacs/.emacs.el
+++ stow/emacs/.emacs.el
@@ -4,9 +4,12 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("8e797edd9fa9afec181efbfeeebf96aeafbd11b69c4c85fa229bb5b9f7f7e66c" "2b9dc43b786e36f68a9fd4b36dd050509a0e32fe3b0a803310661edb7402b8b6" default)))
  '(package-selected-packages
    (quote
-    (emmet-mode zencoding-mode rjsx-mode treemacs yasnippet-snippets yasnippet scss-mode use-package rg rainbow-mode rainbow-delimiters projectile powerline org-bullets olivetti markdown-mode magit js2-mode hlinum gruvbox-theme general flx fill-column-indicator evil-surround diminish diff-hl counsel))))
+    (origami emmet-mode zencoding-mode rjsx-mode treemacs yasnippet-snippets yasnippet scss-mode use-package rg rainbow-mode rainbow-delimiters projectile powerline org-bullets olivetti markdown-mode magit js2-mode hlinum gruvbox-theme general flx fill-column-indicator evil-surround diminish diff-hl counsel))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
